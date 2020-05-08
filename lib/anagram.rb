5627ae7e5fd4abc54afeb941745ffda859db23de
Thomas John Helin <thomashelin@yahoo.com>
2020-05-08 17:05:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
  end
 
  def match(word_array)
-   word_array.select do |word| #.select is great when you want to filter a list & get an array with the results
+   word_array.select do |word| #.select is great when you want to filter a list & get an array with the results. It starts a loop.
      word.split("").sort == @word.split("").sort #.split cuts up a string into pieces along a pre-defined string or regex, returning an array of smaller strings.. #.sort returns a copy of an original array.
    end
  end
